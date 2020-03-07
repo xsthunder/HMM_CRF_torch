963f2b05931c815a10e4598ca593d9d1d1fa3508
xsthunder <xsthunder@outlook.com>
2020-03-07 19:11:32 +0800

    fix travis

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,14 @@
 set -e
+wget baidu.com -P ../data/HMM
+#wget https://raw.githubusercontent.com/fxsjy/jieba/master/jieba/dict.txt -P ../data/HMM
+
+# https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+# TRAVIS=TRUE
+if test $TRAVIS
+then
+    source $HOME/miniconda/etc/profile.d/conda.sh
+    conda activate py3torch-cpu
+fi
 
 for file in ./*.py
 do
