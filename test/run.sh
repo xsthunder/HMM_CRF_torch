889da158915d1c9ea3e86512f875177b49331111
xsthunder <xsthunder@outlook.com>
2020-03-08 10:09:54 +0800

    add ResumeNER data

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,21 @@
 set -e
 
-wget https://raw.githubusercontent.com/fxsjy/jieba/master/jieba/dict.txt -P ../data/HMM
+# download data
 
+## for data/CRF/ResumeNER
+
+p='../data/CRF/ResumeNER'
+
+for file in $(cat $p/data-source-list.txt)
+do
+	wget $file -P $p
+done
+
+## for data/HMM
+wget https://raw.githubusercontent.com/xsthunder/jieba/master/jieba/dict.txt -P ../data/HMM
+
+
+# set up conda env
 # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
 # TRAVIS=TRUE
 if test $TRAVIS
@@ -10,6 +24,8 @@ then
     conda activate py3torch-cpu
 fi
 
+
+# run test
 for file in ./*.py
 do
 	python $file
