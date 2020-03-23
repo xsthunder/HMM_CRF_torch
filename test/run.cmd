b8a15dfc70227fd6dd1cb4487aeffd5b2e268d05
xsthunder <xsthunder@outlook.com>
2020-03-23 20:00:19 +0800

    update test config

--- test/run.cmd
+++ test/run.cmd
@@ -1,6 +1,6 @@
 :: print(sys.executable)
 :: C:\Users\xiaos\Anaconda3\envs\py3torch-cpu\python.exe test_run_all_tests.py
-
+set TRAVIS=true
 
 :: https://stackoverflow.com/questions/138497/iterate-all-files-in-a-directory-using-a-for-loop
 ::https://stackoverflow.com/questions/734598/how-do-i-make-a-batch-file-terminate-upon-encountering-an-error
@@ -10,7 +10,6 @@ for %%f in (.\*.py) do (
   echo "-------------------------------------------------------------------------------------" "checking: %%f"
   C:\Users\xiaos\Anaconda3\envs\py3torch-cpu\python.exe %%f || goto :error
 )
-
 
 goto :EOF
 
