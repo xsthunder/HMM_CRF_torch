859e8c16405b30a91848debb9b9f49ec3bb9c204
xsthunder <xsthunder@outlook.com>
2020-03-23 21:01:10 +0800

    remove naive viterbi

--- exp/CRF.py
+++ exp/CRF.py
@@ -29,74 +29,13 @@ def onehot(y, dim_size):
     eye = np.eye(dim_size)
     return eye[y]
 
-import sure
-from queue import PriorityQueue
-class TopK:
-    def __init__(self, k, topks = None):
-        # (priority_number, data)
-#         pq = PriorityQueue(k)
-        assert k == 1, "not implemented"
-
-        self.m_inf = -float('inf')
-        self.data = None
-        self.score = self.m_inf
-
-        if topks is not None:
-            assert len(topks) >= k
-            for t in topks:
-                self.put( t.get_max_data(), t.get_max_score(),)
-            return
-
-#         for i in range(k):
-#             pq.put_nowait((m_inf))
-#         self.pq = pq
-
-    def put(self,  data, score,):
-        assert len(data) >= 1
-        if self.score is self.m_inf:
-            pass
-        else :
-            assert type(score) == type(self.score), "score type muse consit"
-        if score > self.score:
-            self.score, self.data = score, data
-
-    def __str__(self):
-        return (self.score, self.data).__str__()
-
-    def get_max_score(self):
-        return self.score
-
-    def get_max_data(self):
-        return self.data
-
-    def get_kth_data(self, k):
-        raise NotImplementedError
-
-    def get_kth_score(self, k):
-        raise NotImplementedError
-
-    def __repr__(self):
-        return self.__str__()
-
-
-
-topk = TopK(1)
-for i in range(10):
-    topk.put( [i],i+10,)
-#     print(topk.data)
-# assert topk.
-topk.get_max_data().should.eql([9])
-topk.get_max_score().should.eql(19)
-
 def j_viterbi(logits, transitions):
     """
     Uses viterbi algorithm to find most likely tags for the given inputs.
     If constraints are applied, disallows all other transitions.
     Returns a tensor with the same size as the mask (not list)
     """
-    logits = model.nodes
     mask = None
-    transitions = model.crf.trans
     batch_size, sequence_length, num_tags = logits.size()
 
     if mask is None:
@@ -141,56 +80,6 @@ def j_viterbi(logits, transitions):
         best_path.insert(0, indices)
     best_path = torch.stack(best_path).transpose(0, 1)
     return best_path
-
-# dont use this
-def viterbi(nodes,trans_p, initial_state = None, topk = 1, start_p = None):
-    """
-    nodes: array, [{"<TAG>": <float>}]
-    trans_p: dict, {"<TAG_A>": {"TAG_B": <float>}}, TAG_A -> TAG-B
-    return_max=True: bool, set to False if your want to avoid ill endding tag and find your own.
-    initial_state=None: None or list to avoid impossible starting tags
-    start_p: P(o1), may be nessary
-    all float number in log form
-    """
-    assert len(nodes.shape) == 2,len(nodes.shape)  # max_seq_len, label_num
-    kth = 1
-    if initial_state is None:
-        initial_state = nodes[0]
-        nodes = nodes[1:]
-
-    def init_tok():return TopK(kth)
-
-    def init_toks(): return [init_tok() for i in range(nodes.shape[-1])]
-
-    last_best_nodes = init_toks()
-    next_best_nodes = init_toks()
-
-
-    for idx, (tk,score) in enumerate( zip(last_best_nodes , initial_state)):
-        tk.put([idx], score)
-
-    for node in nodes:
-        for t,v in enumerate(node):
-
-            current_best_node = init_tok() # for t
-
-            for topk in last_best_nodes:
-                path = topk.get_max_data()
-                score = topk.get_max_score()
-
-                last_t = path[-1]
-                new_path = path + [t]
-                tran = trans_p[last_t][t]
-
-                current_best_node.put(new_path, score + tran + v)
-
-            next_best_nodes[t] = current_best_node
-
-        next_best_nodes, last_best_nodes =  last_best_nodes, next_best_nodes,
-
-    topk = TopK(1, last_best_nodes)
-
-    return topk.get_max_data()
 
 def get_shift_mask(labels):
     """
